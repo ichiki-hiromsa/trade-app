a329bb46637ce73477cefe1c1219b80f4b0b788c
ichiki-hiromsa <70831210+ichiki-hiromsa@users.noreply.github.com>
2020-10-04 17:43:58 +0900

    1

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,32 @@
 <template>
-  <v-row justify="center" align="center" class="grey lighten-3">
-    <v-col v-for="icon in icons" :key="icon" cols="12" sm="6" md="4">
-      <img :src="icon.img" height="100" />
-      <!-- <img src="/img/img1.jpg" height="200"> -->
-    </v-col>
+  <div justify="center" align="center">
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />      
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn color="white" class="black--text">
+        ログイン
+      </v-btn>
+    </v-app-bar>
+
+    <v-row justify="center" align="center" class="grey lighten-3">
+      <v-col v-for="(icon,index) in icons" :key="index" cols="12" sm="6" md="4">
+        <v-card height="150px" width="200px">
+          <img :src="icon.img" height="100px" width="150px"/>
+          <!-- <img src="/img/img1.jpg" height="200"> -->
+        </v-card>
+      </v-col>
 
 
 
-  </v-row>
+    </v-row>
+    <v-card-actions class="justify-center">
+      <v-btn>
+        トレードに出す
+      </v-btn>
+    </v-card-actions>
+
+  </div>  
 </template>
 
 <script>
@@ -16,6 +35,14 @@ export default {
   data () {
     return {
       icons:[
+        {
+          img:'/img/img1.jpg',
+          to:'/'
+        },
+        {
+          img:'/img/img1.jpg',
+          to:'/'
+        },
         {
           img:'/img/img1.jpg',
           to:'/'
