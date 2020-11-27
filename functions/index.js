8f7bb3314331abd6a88b6a62896cffe7ff6771f0
ichiki-hiromsa <70831210+ichiki-hiromsa@users.noreply.github.com>
2020-11-27 19:33:20 +0900

    1

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,23 @@
 const functions = require('firebase-functions');
 
-var admin = require('firebase-admin');
+const admin = require('firebase-admin');
+const db =admin.firestore();
+admin.initializeApp();//
+
 var serviceAccount = require("./trade-app-ichi-firebase-adminsdk-5kl3n-05fe62ac12.json"); // 自分の鍵jsonファイル
 
-var defaultApp = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://trade-app-ichi.firebaseio.com'
-});//DBURLは固有のもの
-
-console.log(defaultApp.name);  // "[DEFAULT]になっている"
+//外部から呼び出すときはこっち？\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// var defaultApp = admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://trade-app-ichi.firebaseio.com'
+// });//DBURLは固有のもの
 
 // Auth関連
 var defaultAuth = defaultApp.auth();
 
 // Database関連
-var defaultDatabase = defaultApp.database();
+// var defaultDatabase = defaultApp.database();
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -24,3 +27,10 @@ var defaultDatabase = defaultApp.database();
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+exports.testfunction = functions.https.onRequest((request, response) => {
+  db.doc("/users/oYljL6ibEQRk3AKnNks0").get()
+  ,then(doc=>{
+    const data = doc.data()
+    response.send(data);
+  })
+})
